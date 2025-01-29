49cc5ba2d31d3b9873997e91f8883529672abfbc
Langen <langen107@gmail.com>
2025-01-29 18:59:01 +0100

    Created Edit function,

---
+++ RestaurantWebAppProject/Migrations/20250128193643_produkt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantWebAppProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class produkt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
